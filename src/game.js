d32a72f4ef9313aa80ffef48ca216c4e157856b1
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-15 22:34:28 +0100

    add some starter code

---
+++ src/game.js
@@ -0,0 +1,8 @@
+class technician{
+    constructor(answer1, answer2){
+       this.salary=100;
+       this.happyCustomers=[];
+
+    }
+}
+
---
+++ src/script.js
@@ -0,0 +1,30 @@
+const questions=[
+    {problem:'Certain keys on my keyboard are not working',  
+    answer1:'', 
+    answer2:''},
+    {problem:'My laptop does not start',  
+    answer1:'', 
+    answer2:''},
+    {problem:'My laptop fell down, the screen is cracked',  
+    answer1:'', answer2:''},
+    {problem:'The touchpad causes the cursor to jump randomly',  
+    answer1:'', answer2:''},
+    {problem:'My battery does not charge',  
+    answer1:'', 
+    answer2:''},
+    {problem:'My laptop overheats after a while, the fan is not working',  
+    answer1:'', 
+    answer2:''},
+    {problem:'Speaker issues or no sound coming from speakers.',  
+    answer1:'', answer2:''},
+    {problem:'My laptop crashes before loading the OS',  
+    answer1:'', answer2:''},
+    {problem:'My camera is not working',  
+    answer1:'', answer2:''},
+    {problem:'My mouse is not working',  answer1:'', answer2:''}, 
+]
+
+const parts=[
+
+]
+
