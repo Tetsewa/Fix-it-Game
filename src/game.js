573f67526172f460718da83bce64adf61829f9d9
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-17 11:01:49 +0100

    describe the classes and methods

--- src/game.js
+++ src/game.js
@@ -1,5 +1,53 @@
-class technician{
-    constructor(solution1, solution2){
+
+class Game {
+    constructor(){
+
+    }
+
+//defining the method to start the game
+    startGame(){
+        //hide screen one(yet to assign class)
+
+        //load screen two(yet to be defined)
+
+
+        //load each problem in a div to be created
+
+
+        //load allAnswers for that problem
+
+
+        
+    }
+
+    checkCorrectAnswers(){
+        //checks that index of the answers selected by player are the same as the problem's index
+    }
+
+    endGame(){}
+
+
+
+
+    //method to restart the game after game ends
+    restartGame(){
+        //load screen one (yet to assign class)
+        
+        //define salary back to 100
+
+        //empty happy customers arrray
+    }
+
+    
+
+
+
+
+}
+
+
+class player{
+    constructor(){
        this.salary=100;
        this.happyCustomers=[];
 
--- src/script.js
+++ src/script.js
@@ -1,33 +1,81 @@
-const questions=[
+const myQuestions=[
     {problem:'While closing my laptop, the hinges broke and the screws fell off',  
-    solution1:'', 
-    solution2:''},
+    allAnswers:['hinges.jpg', 'screws.jpg','motherboard.jpg','hard-disk-drive.jpg'], 
+    correctAnswers:['hinges.jpg', 'screws.jpg']},
+
     {problem:'My laptop does not start',  
-    solution1:'', 
-    solution2:''},
+    allAnswers:['motherboard.jpg', 'charger.jpg','speakers.jpg','camera-board.jpg'], 
+    correctAnswers:['motherboard.jpg', 'charger.jpg']},
+
     {problem:'My laptop fell down, the screen is cracked',  
-    solution1:'', solution2:''},
+    allAnswers:['screen.jpg','LCD-cover.jpg','speakers.jpg','hard-disk-drive.jpg'], 
+    correctAnswers: ['screen.jpg','LCD-cover.jpg']},
+
     {problem:'The touchpad causes the cursor to jump randomly',  
-    solution1:'', solution2:''},
+    allAnswers:['touchpad.jpg','touchpad-cable.jpg','screws.jpg','camera-board.jpg'] ,
+    correctAnswers:['touchpad.jpg','touchpad-cable.jpg']},
+
     {problem:'My battery does not charge',  
-    solution1:'', 
-    solution2:''},
+    allAnswers:['battery.jpg','charger.jpg'], 
+    correctAnswers: ['battery.jpg','charger.jpg', 'LCD-cover.jpg', 'camera-board.jpg']},
+
     {problem:'My laptop overheats after a while, the fan is not working',  
-    solution1:'', 
-    solution2:''},
+    allAnswers:['fan.jpg','motherboard.jpg','screen.jpg','speakers.jpg'], 
+    correctAnswers: ['fan.jpg','motherboard.jpg']},
+
     {problem:'Speaker issues or no sound coming from speakers.',  
-    solution1:'', solution2:''},
+    allAnswers:['speakers.jpg','motherboard.jpg', 'mouse.jpg','battery.jpg'], 
+    correctAnswers: ['speakers.jpg','motherboard.jpg' ]},
+
     {problem:'My laptop crashes after the logo appears',  
-    solution1:'', solution2:''},
+    allAnswers:['hard-disk-drive.jpg','motherboard.jpg','mouse.jpg','speakers.jpg'], 
+    correctAnswers: ['hard-disk-drive.jpg','motherboard.jpg']},
+
     {problem:'My camera is not working',  
-    solution1:'', solution2:''},
-    {problem:'My mouse is not working when I connect it to my laptop',  solution1:'', solution2:''}
+    allAnswers:['camera-board.jpg','camera-cable.jpg','charger.jpg','battery.jpg'], 
+    correctAnswers: ['camera-board.jpg','camera-cable.jpg']},
+
+    {problem:'My mouse is not working when I connect it to my laptop',  
+    allAnswers:['USB-port.jpg','mouse.jpg','LCD-cover.jpg','hinges.jpg'], 
+    correctAnswers: ['USB-port.jpg','mouse.jpg']},
 ]
 
+
+//instantiation
+
+
+
+
+//selecting all objects
+startGame=document.querySelector('.start-btn')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const spareParts=[
     { name: 'battery', img: 'battery.jpg' },
     { name: 'camera-board', img: 'camera-board.jpg' },
     { name: 'camera-cable', img: 'camera-cable.jpg' },
+    { name: 'charger', img: 'charger.jpg' },
     { name: 'fan', img: 'fan.jpg' },
     { name: 'hard-disk-drive', img: 'hard-disk-drive.jpg' },
     { name: 'hinges', img: 'hinges.jpg' },
@@ -42,9 +90,4 @@ const spareParts=[
     { name: 'touchpad', img: 'touchpad.jpg' },
     { name: 'USB-port', img: 'USB-port.jpg' }
 ]
-
-//instantiation
-
-
-//selecting all objects
-startGame=document.querySelector('.start-btn')
+*/
