3bf3dcc709f236e9680967ea028082102c55d756
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-17 16:58:26 +0100

    add methods and classes

--- src/game.js
+++ src/game.js
@@ -1,28 +1,74 @@
 
 class Game {
     constructor(){
+        this.screenOne = document.querySelector('#screen-one');
+        this.screenTwo = document.querySelector('#screen-two');
+        this.screenThree = document.querySelector('#screen-three');
+        this.salary = 100;
+        this.happyCustomers = [];
+        this.gameIsOver = false;
+        this.myQuestions=myQuestions;
+        this.problemDisplayed = document.querySelector('.problem-displayed');
 
     }
+
+
+    problemDisplayed=document.querySelector('.problem-displayed');
+
 
 //defining the method to start the game
     startGame(){
-        //hide screen one(yet to assign class)
+        //hide screen one
+        this.screenOne.style.display = "none";
 
-        //load screen two(yet to be defined)
+        //load screen two
+        this.screenTwo.style.display = "block";
 
+        //load each problem from the MyQuestions array in the problemDisplayed div
+        //BELOW NOT WORKING!!
 
-        //load each problem in a div to be created
+        //loadProblem(element);
 
-
+        //OR
+        // this.myQuestions.forEach((element )=> {
+        //     html= `<h3> ${this.myQuestions[i].problem}</h3>`
+        // });
+        // problemDisplayed.innerHTML = html
+    
         //load allAnswers for that problem
-
+        //loadAllAnswers
 
         
     }
+    
+   
 
+
+   
     checkCorrectAnswers(){
-        //checks that index of the answers selected by player are the same as the problem's index
+        //checks that index of the answers clicked by player are the same as the problem's index
+
+        //define the click event in the script.js and call here
+
+        
+        
     }
+
+    checkIfWon(){
+        //if each checkCorrectAnswers is true,
+
+        //add 10 to the salary
+        this.salary+=10;
+
+        //add a happy face to the array
+        this.happyCustomers.push('happy-face.png')
+
+
+
+        //if each checkCorrectAnswers is true,
+    }
+
+  
 
     endGame(){}
 
@@ -31,11 +77,13 @@ class Game {
 
     //method to restart the game after game ends
     restartGame(){
-        //load screen one (yet to assign class)
-        
+        //load screen one 
+        this.screenOne.style.display = "block";
         //define salary back to 100
-
+        this.salary = 100;
         //empty happy customers arrray
+        this.happyCustomers = [];
+
     }
 
     
@@ -46,11 +94,4 @@ class Game {
 }
 
 
-class player{
-    constructor(){
-       this.salary=100;
-       this.happyCustomers=[];
-
-    }
-}
 
---
+++ src/questions.js
@@ -0,0 +1,46 @@
+const myQuestions=[
+    {problem:'While closing my laptop, the hinges broke and the screws fell off',  
+    allAnswers:['hinges.jpg', 'screws.jpg','motherboard.jpg','hard-disk-drive.jpg'], 
+    correctAnswers:['hinges.jpg', 'screws.jpg']},
+
+    {problem:'My laptop does not start',  
+    allAnswers:['motherboard.jpg', 'charger.jpg','speakers.jpg','camera-board.jpg'], 
+    correctAnswers:['motherboard.jpg', 'charger.jpg']},
+
+    {problem:'My laptop fell down, the screen is cracked',  
+    allAnswers:['screen.jpg','LCD-cover.jpg','speakers.jpg','hard-disk-drive.jpg'], 
+    correctAnswers: ['screen.jpg','LCD-cover.jpg']},
+
+    {problem:'The touchpad causes the cursor to jump randomly',  
+    allAnswers:['touchpad.jpg','touchpad-cable.jpg','screws.jpg','camera-board.jpg'] ,
+    correctAnswers:['touchpad.jpg','touchpad-cable.jpg']},
+
+    {problem:'My battery does not charge',  
+    allAnswers:['battery.jpg','charger.jpg'], 
+    correctAnswers: ['battery.jpg','charger.jpg', 'LCD-cover.jpg', 'camera-board.jpg']},
+
+    {problem:'My laptop overheats after a while, the fan is not working',  
+    allAnswers:['fan.jpg','motherboard.jpg','screen.jpg','speakers.jpg'], 
+    correctAnswers: ['fan.jpg','motherboard.jpg']},
+
+    {problem:'Speaker issues or no sound coming from speakers.',  
+    allAnswers:['speakers.jpg','motherboard.jpg', 'mouse.jpg','battery.jpg'], 
+    correctAnswers: ['speakers.jpg','motherboard.jpg' ]},
+
+    {problem:'My laptop crashes after the logo appears',  
+    allAnswers:['hard-disk-drive.jpg','motherboard.jpg','mouse.jpg','speakers.jpg'], 
+    correctAnswers: ['hard-disk-drive.jpg','motherboard.jpg']},
+
+    {problem:'My camera is not working',  
+    allAnswers:['camera-board.jpg','camera-cable.jpg','charger.jpg','battery.jpg'], 
+    correctAnswers: ['camera-board.jpg','camera-cable.jpg']},
+
+    {problem:'My mouse is not working when I connect it to my laptop',  
+    allAnswers:['USB-port.jpg','mouse.jpg','LCD-cover.jpg','hinges.jpg'], 
+    correctAnswers: ['USB-port.jpg','mouse.jpg']},
+]
+
+//I don't need the array of faces
+// const faces=[
+//     {name:'happy-face', img: 'happy-face.jpg'}
+// ]
